9a5cc0979ed644d7fd22f38bf4ae29a4d926a2c5
오연주 <ohyj@brandi.co.kr>
2018-10-21 12:58:04 +0900

    get product low price

--- get_low_price.py
+++ get_low_price.py
@@ -2,7 +2,6 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 def get_low_price(url):
-    url = 'https://search.shopping.naver.com/detail/detail.nhn?nv_mid=12804648154&cat_id=50000391&frm=NVSHMDL&query=&NaPm=ct%3Djmwz6vdc%7Cci%3D2b003e3024fa1a918ef1b431f6f3cdceafc32fbd%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D253e7b00d1ab4680dab951fe67baa41eb224824b'
     with urllib.request.urlopen(url) as response:
         # read decoded html code
         html = response.read().decode('utf-8')
@@ -17,3 +16,4 @@ def get_low_price(url):
         re2 = re[0].find('em', class_='num').string
         re2 = int(re2.replace(',', ''))
 
+        return re2
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from get_low_price import get_low_price
+
 def main():
-    f = open('./list.text', 'w')
-    
+    with open('./list.txt', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            low_price = get_low_price(line)
+            
+
+if __name__ == '__main__':
+    main()
+
